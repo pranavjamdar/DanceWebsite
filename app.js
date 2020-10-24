f560ca7a3709ff77ff7518b2198bc64a4153e726
rutujajamdar <68149595+rutujajamdar@users.noreply.github.com>
2020-10-24 18:45:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const bodyparser = require("body-parser");
 
 mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
-const port = 8000;
+const port = 9000;
 
 
 // Define mongoose schema
